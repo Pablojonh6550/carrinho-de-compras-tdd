791d878af02d3fcbb735911244581c51f462a774
Pablojonh6550 <pablojonh6550@gmail.com>
2025-06-16 18:31:31 -0300

    feat: add select payment method case

---
+++ app/Domain/Payment/Enums/PaymentMethod.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Domain\Payment\Enums;
+
+enum PaymentMethod: string
+{
+    case PIX = 'PIX';
+    case CREDIT_CARD_ONE_TIME = 'CREDIT_CARD_ONE_TIME';
+    case CREDIT_CARD_INSTALLMENTS = 'CREDIT_CARD_INSTALLMENTS';
+
+    public function getStrategy(): string
+    {
+        return match ($this) {
+            self::PIX => \App\Domain\Payment\Methods\PixPayment::class,
+            self::CREDIT_CARD_ONE_TIME => \App\Domain\Payment\Methods\CreditCardOneTime::class,
+            self::CREDIT_CARD_INSTALLMENTS => \App\Domain\Payment\Methods\CreditCardInstallments::class,
+        };
+    }
+}
