37405369295de192cc73c1ebc9cdcaa727aa6080
Pablojonh6550 <pablojonh6550@gmail.com>
2025-06-18 17:59:50 -0300

    refactor: add error treatment

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -1,8 +1,13 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use App\Services\Cart\CartService;
+use Exception;
+use App\Http\Requests\PaymentRequest;
+use Illuminate\Support\Facades\Log;
 
 class CartController extends Controller
 {
@@ -11,13 +16,45 @@ class CartController extends Controller
     public function finishCart(): JsonResponse
     {
         $cart = [
-            'items' => [],
-            'method_payment' => 0,
-            'installments' => 0
+            'products' => [
+                [
+                    'id' => 1,
+                    'name' => 'leite',
+                    'value' => '1000',
+                    'quantity' => 1
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'arroz',
+                    'value' => '2000',
+                    'quantity' => 1
+                ],
+                [
+                    'id' => 3,
+                    'name' => 'feijao',
+                    'value' => '3340',
+                    'quantity' => 1
+                ]
+            ],
+            'method_payment' => 'CREDIT_CARD_INSTALLMENTS',
+            'installments' => 3
         ];
+        try {
 
-        $this->cartService->finishCart($cart['items'], $cart['method_payment'], $cart['installments']);
+            $result = $this->cartService->finishCart($cart['products'], $cart['method_payment'], $cart['installments']);
+            if ($result)
+                return response()->json(['message' => 'Pagamento realizado com sucesso', 'data' => ['total_value' => $result]], 200);
+            return response()->json(['message' => 'não foi possivel realizar o pagamento', 'data' => $result], 400);
+        } catch (Exception $e) {
+            Log::error('Erro ao tentar realizar pagamento', [
+                'paymeny_method' => $cart['method_payment'],
+                'exception' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+            ]);
 
-        return response()->json(['success' => true]);
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 }
